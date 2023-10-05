c51a7c2281c26dcf22598e35651f85111b12a6b6
liutc-1203 <tl17illinois@gmail.com>
2023-10-04 21:04:10 -0500

    mp2.2

--- search_eval.py
+++ search_eval.py
@@ -11,7 +11,8 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25()
+    return metapy.index.OkapiBM25(k1=1.5, b=0.75, k3 = 3.23)
+    # return metapy.index.PivotedLength(); 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
